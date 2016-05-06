2c2548b4f51fbf7cd1c70e2735c7960cfa0caccf
Souline <soline.aubert@insa-lyon.fr>
2016-05-06 12:53:13 +0200

    Update serveur.h

--- serveur.h
+++ serveur.h
@@ -8,20 +8,19 @@
 #include <pthread.h>
 #include <signal.h>
 
-#define MSS 1030 //max segment size
+#define MSS 1036 //max segment size
 #define MDS 1024 // max data size
-
+#define H_SIZE 12 //header size
 #define NUMSEQ_SIZE 6 //nb bytes for num segment in header
-
-
+#define FRAGM_FLAG_SIZE 1 //nb bytes for fragm flag in header
+#define DATA_SIZE 5 // nb bytes for size in header
 #define DUPLICATE 3
 #define TRUE 1
 #define FALSE 0
-#define RWND 30 //by testing with a fixed cwnd we found that rwnd should be around 32
 
 struct sockaddr_in serveur, client, data;
 int port, port_data, connected,valid,count, nbClient;
-
+int fragm; //fragm : 1 if the seq is not the last seq ; 0 if it's the last seq
 int desc,desc_data_sock;
 int msgSize;
 int file_size;
@@ -31,18 +30,16 @@ char recep[MSS], sndBuf[MSS];
 socklen_t alen;
 FILE* fin;
 
-double cwnd;
+int cwnd;
 int flight_size;
 int okFile;
-int debug;
 
 pthread_mutex_t mutex;
 pthread_t listener,sender;//2 thread one to send the file the other to receive ACKs
 pthread_attr_t attr_listener,attr_sender;
 
 void connexion();
-int checkACK();
-char *str_sub ( int start,  int end);
+void new_connexion();
 void init();
 void conversation();
 void *send_file(void *arg);
