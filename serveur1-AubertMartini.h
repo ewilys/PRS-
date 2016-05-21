34e7544fe94dc32a8dc67bd18d4526b5e6551c3c
Souline <soline.aubert@insa-lyon.fr>
2016-05-21 21:01:16 +0200

    Update serveur1-AubertMartini.h

--- serveur1-AubertMartini.h
+++ serveur1-AubertMartini.h
@@ -7,14 +7,19 @@
 #include <netinet/in.h>
 #include <pthread.h>
 #include <signal.h>
+#include <time.h>
+#include <sys/select.h>
+#include <sys/time.h>
 
-#define MSS 1030 //max segment size
-#define MDS 1024 // max data size
+#define MSS 1500 //max segment size
+#define MDS 1494 // max data size
 
 #define NUMSEQ_SIZE 6 //nb bytes for num segment in header
 
+#define MAX_SIZE_BUFCIRCULAIRE 250000
+#define DUPLICATE 3
+#define RWND 60
 
-#define DUPLICATE 2
 #define TRUE 1
 #define FALSE 0
 
@@ -29,10 +34,14 @@ char recep[MSS], sndBuf[MSS];
 socklen_t alen;
 FILE* fin;
 
-int cwnd;
-int flight_size;
+struct timeval timeout, start, end, t_elapsed, RTT; 
+long cste = 0.5;    
+    
+double cwnd;
+double flight_size;
 int okFile;
 int debug;
+double ssthresh;
 
 pthread_mutex_t mutex;
 pthread_t listener,sender;//2 thread one to send the file the other to receive ACKs
@@ -43,6 +52,8 @@ int checkACK();
 char *str_sub ( int start,  int end);
 void init();
 void conversation();
+double min(double, double);
 void *send_file(void *arg);
 void *receive_ACK(void *);
 int catch_file_size();
+struct timeval estimateRTT(struct timeval, long, struct timeval);
